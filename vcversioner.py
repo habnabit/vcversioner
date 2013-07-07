5574c01d524ba8e6dded579f1d55775df39acdf1
Aaron Gallagher <habnabit@gmail.com>
2013-07-06 20:28:08 -0700

    Make vcversioner.py better for bundling.

--- vcversioner.py
+++ vcversioner.py
@@ -1,7 +1,23 @@
-# Copyright (c) Aaron Gallagher <_@habnab.it>
-# See COPYING for details.
+# Copyright (c) 2013, Aaron Gallagher <_@habnab.it>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
 
-"Simplify your python project versioning."
+"""Simplify your python project versioning.
+
+In-depth docs online: https://vcversioner.readthedocs.org/en/latest/
+Code online: https://github.com/habnabit/vcversioner
+
+"""
 
 from __future__ import print_function, unicode_literals
 
